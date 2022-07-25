2634d32da85cdecb8e99b1c5ae670fe79d1b14ec
Gallie83 <102595438+Gallie83@users.noreply.github.com>
2022-07-25 16:03:53 +0100

    Created initial battleship board

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+board = []
+
+x_axis = [" ", "A", "B", "C", "D", "E", "F"]
+
+
+def create_board():
+    """
+    Creates the board at the start of a new round
+    """
+    board.append(x_axis)
+    for i in range(0, 5):
+        board.append([f"{i + 1}"] + ["O"] * 6)
+
+
+create_board()
+
+
+def stack_board(board):
+    """
+    Stacks the board into equal columns and rows
+    """
+    for i in board:
+        print(i)
+
+
+stack_board(board)
